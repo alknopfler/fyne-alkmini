e99b09d7d6e1576d76783ba145336c9b7f37496a
Alberto Morgante Medina <alknopfler@gmail.com>
2023-05-30 12:07:15 +0200

    add logic to different scenarios

--- alkmini.go
+++ alkmini.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net"
 	"os/exec"
@@ -8,23 +9,59 @@ import (
 )
 
 func getStatus() string {
-	timeout := 3 * time.Second
-	conn, err := net.DialTimeout("tcp", "alkmini:22", timeout)
+	timeout := 2 * time.Second
+	_, err := net.DialTimeout("tcp", "alkmini:22", timeout)
 	if err != nil {
 		log.Println("host unreachable, error: ", err.Error())
-		return "Host unreachable"
+		return STATUS_DOWN
 	}
-	defer conn.Close()
-	return "Host up and running"
+	return STATUS_UP
 }
 
 func startServer() error {
-	cmd := exec.Command("alkmini-wake")
-	err := cmd.Run()
+	cmd := exec.Command("/Users/alknopfler/bin/alkmini-wake")
+
+	_, err := cmd.Output()
 
 	if err != nil {
-		log.Println("Error starting server: ", err.Error())
+		log.Println("Error executing command startServer: ", err.Error())
 		return err
 	}
 	return nil
+}
+
+func stopServer() error {
+	cmd := exec.Command("ssh", "alkmini", "sudo systemctl suspend")
+	err := cmd.Run()
+	if err != nil {
+		log.Println("Error executing command stopServer: ", err.Error())
+		return err
+	}
+	return nil
+}
+
+func createTunnel() error {
+	if getStatus() != STATUS_UP {
+		log.Println("Cannot create tunnel, server is down")
+		return errors.New("Cannot create tunnel, server is down")
+	}
+	cmd := exec.Command("sshuttle", "-D", "-r", "alkmini", "192.168.122.0/24")
+	err := cmd.Run()
+	if err != nil {
+		log.Println("Error executing command createTunnel: ", err.Error())
+		return err
+	}
+	return nil
+}
+
+func waitUntilUp() {
+	for getStatus() != STATUS_UP {
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func waitUntilDown() {
+	for getStatus() != STATUS_DOWN {
+		time.Sleep(5 * time.Second)
+	}
 }
--- main.go
+++ main.go
@@ -8,40 +8,81 @@ import (
 	"time"
 )
 
+const (
+	PROGNAME              = "Alkmini Management System"
+	STATUS_UP             = "Server Status: Host up and running"
+	STATUS_DOWN           = "Server Status: Host down"
+	STATUS_TUNNELED       = "Server Status: Tunnel running"
+	PROCESS_START         = "Process: Starting Server"
+	PROCESS_STARTED       = "Process: Server started"
+	PROCESS_START_ERROR   = "Process: Server start error"
+	PROCESS_SUSPENDED     = "Process: Server suspended"
+	PROCESS_SUSPEND_ERROR = "Process: Server suspend error"
+	PROCESS_STOP          = "Process: Stopping Server"
+	PROCESS_TUNNEL        = "Process: Tunneling"
+	PROCESS_TUNNELED      = "Process: Tunnel running"
+	PROCESS_TUNNEL_ERROR  = "Process: Tunnel error"
+	HIDDEN                = ""
+	GETSERVERSTATUSLABEL  = "Get Server Status"
+	STARTSERVERLABEL      = "Start Server"
+	STOPSERVERLABEL       = "Stop Server"
+	TUNNELLABEL           = "Tunnel sshuttle"
+)
+
 func main() {
 	a := app.New()
-	w := a.NewWindow("Alkmini Management system")
-	label := widget.NewLabel("Alkmini Management System")
-	status := widget.NewLabel("")
-	process := widget.NewLabel("")
+	w := a.NewWindow(PROGNAME)
+	label := widget.NewLabel(PROGNAME)
+	status := widget.NewLabel(HIDDEN)
+	process := widget.NewLabel(HIDDEN)
 	w.SetContent(container.NewVBox(
 		label,
 		status,
 		widget.NewSeparator(),
 		process,
 
-		widget.NewButtonWithIcon("Get Server Status", theme.InfoIcon(), func() {
+		widget.NewButtonWithIcon(GETSERVERSTATUSLABEL, theme.InfoIcon(), func() {
 			status.SetText(getStatus())
 		}),
-		widget.NewButtonWithIcon("Start Server", theme.MediaPlayIcon(), func() {
-			process.SetText("Starting Server")
-			startServer()
-			for getStatus() != "Host up and running" {
-				time.Sleep(5 * time.Second)
+		widget.NewButtonWithIcon(STARTSERVERLABEL, theme.MediaPlayIcon(), func() {
+			process.SetText(PROCESS_START)
+			if startServer() != nil {
+				process.SetText(PROCESS_START_ERROR)
+				status.SetText(STATUS_DOWN)
+				return
+			} else {
+				waitUntilUp()
+				process.SetText(PROCESS_STARTED)
+				status.SetText(STATUS_UP)
 			}
-			process.SetText("Server started")
-			status.SetText("Host up and running")
 			time.Sleep(5 * time.Second)
 			process.SetText("")
 
 		}),
-		widget.NewButtonWithIcon("Stop Server", theme.MediaStopIcon(), func() {
-			process.SetText("stopping")
-			status.SetText("inactivo")
+		widget.NewButtonWithIcon(STOPSERVERLABEL, theme.MediaStopIcon(), func() {
+			process.SetText(PROCESS_STOP)
+			if stopServer() != nil {
+				process.SetText(PROCESS_SUSPEND_ERROR)
+				status.SetText(STATUS_UP)
+			} else {
+				waitUntilDown()
+				process.SetText(PROCESS_SUSPENDED)
+				status.SetText(STATUS_DOWN)
+			}
+			time.Sleep(5 * time.Second)
+			process.SetText(HIDDEN)
 		}),
-		widget.NewButtonWithIcon("Tunnel sshuttle", theme.LoginIcon(), func() {
-			process.SetText("tunnel 192.168.122.0/24")
-			status.SetText("Tunnel activo")
+		widget.NewButtonWithIcon(TUNNELLABEL, theme.LoginIcon(), func() {
+			process.SetText(PROCESS_TUNNEL)
+			if createTunnel() != nil {
+				process.SetText(PROCESS_TUNNEL_ERROR)
+				status.SetText(getStatus())
+			} else {
+				process.SetText(PROCESS_TUNNELED)
+				status.SetText(STATUS_TUNNELED)
+			}
+			time.Sleep(5 * time.Second)
+			process.SetText(HIDDEN)
 		}),
 	))
 	w.ShowAndRun()
